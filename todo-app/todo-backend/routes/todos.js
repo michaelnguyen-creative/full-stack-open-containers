b96691ef4f68d3f49de410773cbe5dc83515dd34
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-04-28 12:52:56 +0700

    fix: implement PUT todos/:id route

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require('express')
 const { Todo } = require('../mongo')
-const router = express.Router();
+const router = express.Router()
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
   const todos = await Todo.find({})
-  res.send(todos);
-});
+  res.send(todos)
+})
 
 /* POST todo to listing. */
 router.post('/', async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
+    done: false,
   })
-  res.send(todo);
-});
+  res.send(todo)
+})
 
 // These are just route defs (meaning findByIdMiddleware doesn't work here)
 // Execution happens when you actually use the route
 // Syntax for using the routes: `app.use('/route', router)`
-const singleRouter = express.Router();
+const singleRouter = express.Router()
 
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
@@ -32,20 +32,24 @@ const findByIdMiddleware = async (req, res, next) => {
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
-  res.sendStatus(200);
-});
+  await req.todo.delete()
+  res.sendStatus(200)
+})
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.status(200).json(req.todo); // Implement this
-});
+  res.status(200).json(req.todo)
+})
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+  // You'll need to mutate data in the database
+  req.todo.text = req.body.text
+  req.todo.done = req.body.done
+  const returnedData = await req.todo.save()
+  res.status(200).json(returnedData)
+})
 
 router.use('/:id', findByIdMiddleware, singleRouter)
 
-module.exports = router;
+module.exports = router
