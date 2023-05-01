08c78f9b1957130fb60e3739335365ed641aeec4
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-05-01 16:29:21 +0700

    add redis config to compose file

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const redis = require('../redis')
 
 const configs = require('../util/config')
 
@@ -8,7 +9,7 @@ let visits = 0
 /* GET index data. */
 router.get('/', async (req, res) => {
   visits++
-
+  console.log('redis', JSON.stringify(redis))
   res.send({
     ...configs,
     visits
